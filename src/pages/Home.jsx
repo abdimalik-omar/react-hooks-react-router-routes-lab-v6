aaf627538adfbfbb273be7bb5ca199b2c81cf96c
abdimalik-omar <abdimalik.omar1@student.moringaschool.com>
2024-11-16 01:00:11 +0300

    completed lab

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,36 @@
 import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
+import MovieCard from "../components/MovieCard"; // Ensure this component is correctly imported
 
 function Home() {
+  const [movies, setMovies] = useState([]);
+
+  // Fetching movies when the component mounts
+  useEffect(() => {
+    fetch("http://localhost:4000/movies")
+      .then((response) => response.json())
+      .then((data) => setMovies(data))
+      .catch((error) => console.error("Error fetching movies:", error));
+  }, []);
+
+  // Display a loading state or an empty state if no movies are loaded yet
+  if (movies.length === 0) {
+    return <h1>Loading movies...</h1>;
+  }
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Info goes here! */}
+        <h1>Home Page</h1>
+        {movies.map((movie) => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
       </main>
     </>
   );
-};
+}
 
 export default Home;
